8bcdfecb8e856f3e7db1af24af5531cb7f9f528d
hrushikeshj <hrushi2002j@gmail.com>
2021-01-14 19:02:24 +0530

    changes

--- session_2/cricviz/app/models/cricketer.rb
+++ session_2/cricviz/app/models/cricketer.rb
@@ -11,10 +11,6 @@ class Cricketer < ApplicationRecord
   # Sort players by the descending number of matches played
   scope :descending_by_matches, -> { order( matches: :desc) }
 
-  def cc1
-    #p Cricketer.descending_by_matches
-    
-  end
   # Batting average: Runs scored / (Number of innings in which player has been out)
   #
   # Note:
@@ -28,8 +24,7 @@ class Cricketer < ApplicationRecord
     elsif  not_out == innings_batted
       return runs_scored
     end  
-    Float avg = runs_scored.to_f / ( innings_batted.to_f - not_out.to_f )
-    return avg
+    return runs_scored.to_f / ( innings_batted.to_f - not_out.to_f )
   end
 
   # Batting strike rate: (Runs Scored x 100) / (Balls Faced)
@@ -42,13 +37,13 @@ class Cricketer < ApplicationRecord
     if innings_batted == nil || runs_scored == nil || balls_faced == 0 || balls_faced == nil
       return nil
     end
-    return ( runs_scored * 100 ) /  balls_faced
+    return ( runs_scored.to_f * 100 ) /  balls_faced.to_f
   end
 
   # Create records for the classical batters
   def self.import_classical_batters
     #Name	Country	Role	Matches	Innings	Not Outs	Runs	Balls Faced	High Score	Centuries	Half-Centuries
-    Cricketer.create(name: "Sachin Tendulkar",country: "India",role: "Batter",matches: 200,innings_batted: 329,not_out: 33,runs_scored: 15921,balls_faced: 0,high_score: 248,centuries: 51,half_centuries: 68)
+    Cricketer.create(name: "Sachin Tendulkar",country: "India",role: "Batter",matches: 200,innings_batted: 329,not_out: 33,runs_scored: 15921,balls_faced: nil,high_score: 248,centuries: 51,half_centuries: 68)
     Cricketer.create(name: "Rahul Dravid",country: "India",role: "Batter",matches: 164,innings_batted: 286,not_out: 32,runs_scored: 13288,balls_faced: 31258,high_score: 270,centuries: 36,half_centuries: 63)
     Cricketer.create(name: "Kumar Sangakkara",country: "Sri Lanka",role: "Wicketkeeper",matches: 134,innings_batted: 233,not_out: 17,runs_scored: 12400,balls_faced: 22882,high_score: 319,centuries: 38,half_centuries: 52)
     Cricketer.create(name: "Ricky Ponting",country: "Australia",role: "Batter",matches: 168,innings_batted: 134,not_out: 287,runs_scored: 29,balls_faced: 13378	,high_score: 257,centuries: 41,half_centuries: 62)
@@ -89,7 +84,6 @@ class Cricketer < ApplicationRecord
     batting_scorecard.each do |b|
       if Cricketer.exists?(name: b[0])
         player = Cricketer.find_by(name: b[0])
-        p player
         player.matches += 1
         player.sixes_scored += b[5]
         player.fours_scored += b[4]
