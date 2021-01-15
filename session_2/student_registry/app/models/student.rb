15dd3a884293b5e800e1a246da091e940d28810d
hrushikeshj <hrushi2002j@gmail.com>
2021-01-15 20:41:15 +0530

    validation

--- session_2/student_registry/app/models/student.rb
+++ session_2/student_registry/app/models/student.rb
@@ -1,3 +1,3 @@
 class Student < ApplicationRecord
-    validates :cgpa, numericality: {only_integer: true, less_than_or_equal_to: 10.0, greater_than_or_equal_to: 0.0}
+    validates :cgpa, numericality: {less_than_or_equal_to: 10.0, greater_than_or_equal_to: 0.0}
 end
